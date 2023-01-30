6c39def7128b82dbd9d7d8ae99ae4f8cb39567a2
Mr-PickleVR <122049514+Mr-PickleVR@users.noreply.github.com>
2023-01-30 09:51:04 -0500

    Create pythontest.py

---
+++ pythontest.py
@@ -0,0 +1,11 @@
+data = [1, 2, 3, 4]
+
+
+def data_to_html_table(data):
+    html = '<table><tbody>'
+    for item in data:
+        html += '<tr><td>' + str(item) + '</td></tr>'
+    html += '</tbody></table>'
+    return html
+
+print data_to_html_table(data)
